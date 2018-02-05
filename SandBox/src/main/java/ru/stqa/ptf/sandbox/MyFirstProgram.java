de6ce57b8d98eab3a1faace13000f5a0f566a4a4
KristinaRyz <ryzhikova.kristina@gmail.com>
2018-02-05 12:57:20 +0200

    05.02

--- SandBox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ SandBox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -1,9 +1,22 @@
 package ru.stqa.ptf.sandbox;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class MyFirstProgram{
 
 public static void main (String[] args){
 System.out.println("Hello World!");
+
+    System.out.println(2+2);
+    System.out.println(2*2);
+    System.out.println(2/2);
+    System.out.println(2-2);
+
+    System.out.println("2" + "2");
+
 }
+
+
+
 
 }
